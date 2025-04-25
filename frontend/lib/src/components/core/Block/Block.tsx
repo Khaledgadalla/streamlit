9ffd5a77ac36c4aa05d8bc351882d627e61986c6
Laura Wilby <laura.wilby@snowflake.com>
2025-04-25 21:05:53 +0200

    fix data-testid

--- frontend/lib/src/components/core/Block/Block.tsx
+++ frontend/lib/src/components/core/Block/Block.tsx
@@ -331,7 +331,6 @@ const FlexBoxContainer = (props: FlexBoxContainerProps): ReactElement => {
     height:
       props.node.deltaBlock.flexContainer?.heightConfig?.pixelHeight ||
       undefined,
-    dataTestId: "stVerticalBlockBorderWrapper",
     dataTestScrollBehavior: activateScrollToBottom
       ? "scroll-to-bottom"
       : "normal",
@@ -340,7 +339,10 @@ const FlexBoxContainer = (props: FlexBoxContainerProps): ReactElement => {
   const userKey = getKeyFromId(props.node.deltaBlock.id)
 
   return (
-    <BlockBorderWrapper {...blockBorderWrapperProps}>
+    <BlockBorderWrapper
+      {...blockBorderWrapperProps}
+      data-testid="stVerticalBlockBorderWrapper"
+    >
       <StyledFlexContainerBlock
         {...styles}
         className={classNames(
